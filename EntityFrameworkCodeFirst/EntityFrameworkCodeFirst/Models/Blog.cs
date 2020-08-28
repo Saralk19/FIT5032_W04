deef43ba7dd0187586b467396bf2db7e5129e5e0
saralk1995 <khandelwal.saral2008@gmail.com>
2020-08-28 22:04:44 +1000

    Code First Approach

---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkCodeFirst.Models
+{
+    public class Blog
+    {
+        public int Blogid { get; set; }
+        public string name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+
+    }
+}
